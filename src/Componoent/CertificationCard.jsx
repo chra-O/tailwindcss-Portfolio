86c497f3106d81981fa557fcd5f4d45a6675c3f3
Chra <chra.osman99@gmail.com>
2022-01-26 15:02:41 +0300

    card style finsh

--- src/Componoent/CertificationCard.jsx
+++ src/Componoent/CertificationCard.jsx
@@ -3,17 +3,44 @@ import React from "react";
 export default function CertificationCard() {
   return (
     <>
-      <div className="   inline-flex   w-80 h-52 hover:scale-110 ease-in duration-200  border-4  overflow-hidden bg-blue-800  ">
+      <div className="    inline-flex   m-16  h-64 ease-in duration-200   shadow-xl border-double shadow-neutral-200  overflow-x-hidden ">
         {" "}
         <div>
           {" "}
-          <div className="relative  w-80 h-44   ">
+          <div className="   mb-2 ">
             <img
               src="http://localhost:3000/Image/portfolio.PNG"
-              className="relative"
+              className=" w-full h-44 "
+              alt=""
             ></img>{" "}
-            <div className=" absolute w-80 h-44   hover:opacity-100 "> hi</div>
-          </div>
+            <div className="  flex flex-row mt-2 justify-evenly  mb-2   ">
+              {" "}
+              <a
+                href=" https://chra-o.github.io/chra_othman-Portfolio-/"
+                className="opacity-50 caret-black hover:opacity-100 ease-in duration-200"
+              >
+                <img
+                  className="w-6"
+                  src="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+                  alt=""
+                ></img>
+              </a>
+              <img
+                className=" w-6 h-6  opacity-50 caret-black hover:opacity-100 ease-in duration-200 "
+                src="https://cdn-icons.flaticon.com/png/128/721/premium/721790.png?token=exp=1643198609~hmac=f8751c9ebe65296bbb0457d4c7548675"
+                alt=""
+              ></img>
+              <img
+                className="w-6 h-6 opacity-50 caret-black hover:opacity-100 ease-in duration-200"
+                src=" https://cdn-icons-png.flaticon.com/512/732/732007.png"
+                alt=""
+              ></img>
+            </div>
+            <p className=" mt-2 ml-6 font-thin">
+              {" "}
+              -portfolio project made by css and html
+            </p>
+          </div>{" "}
         </div>
       </div>
     </>
--- src/Componoent/ShowCertification.jsx
+++ src/Componoent/ShowCertification.jsx
@@ -3,7 +3,7 @@ import CertificationCard from "./CertificationCard";
 
 export default function ShowCertification() {
   return (
-    <div className="mt-20 border-red-100 border-double  border-r-8 mr-4 ml-16">
+    <div className="mt-20 border-red-100 border-double  border-r-8 mr-4 ml-28">
       {" "}
       <div className="mt-52  ml-36">
         <p className="sm:text-6xl text-4xl font-thin mb-7 ">Expiryanse:</p>
