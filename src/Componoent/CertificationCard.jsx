919f1fb9e0eb93533c822b32852c461739c6bf10
Chra <chra.osman99@gmail.com>
2022-01-26 20:29:46 +0300

    adding props

--- src/Componoent/CertificationCard.jsx
+++ src/Componoent/CertificationCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function CertificationCard() {
+export default function CertificationCard(props) {
   return (
     <>
       <div className="    inline-flex   m-16  h-64 ease-in duration-200   shadow-xl border-double shadow-neutral-200  overflow-x-hidden ">
@@ -9,37 +9,37 @@ export default function CertificationCard() {
           {" "}
           <div className="   mb-2 ">
             <img
-              src="http://localhost:3000/Image/portfolio.PNG"
+              //
+              src={props.img}
               className=" w-full h-44 "
               alt=""
             ></img>{" "}
             <div className="  flex flex-row mt-2 justify-evenly  mb-2   ">
               {" "}
               <a
-                href=" https://chra-o.github.io/chra_othman-Portfolio-/"
+                href={props.link}
                 className="opacity-50 caret-black hover:opacity-100 ease-in duration-200"
               >
-                <img
-                  className="w-6"
-                  src="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
-                  alt=""
-                ></img>
+                <img className="w-6" src={props.git} alt=""></img>
               </a>
+              
               <img
                 className=" w-6 h-6  opacity-50 caret-black hover:opacity-100 ease-in duration-200 "
-                src="https://cdn-icons.flaticon.com/png/128/721/premium/721790.png?token=exp=1643198609~hmac=f8751c9ebe65296bbb0457d4c7548675"
+                src={props.html}
                 alt=""
               ></img>
               <img
                 className="w-6 h-6 opacity-50 caret-black hover:opacity-100 ease-in duration-200"
-                src=" https://cdn-icons-png.flaticon.com/512/732/732007.png"
+                src={props.css}
+                alt=""
+              ></img>
+              <img
+                className="w-6 h-6 opacity-50 caret-black hover:opacity-100 ease-in duration-200"
+                src={props.php}
                 alt=""
               ></img>
             </div>
-            <p className=" mt-2 ml-6 font-thin">
-              {" "}
-              -portfolio project made by css and html
-            </p>
+            <p className=" mt-2 ml-6 font-thin"> {props.desc}</p>
           </div>{" "}
         </div>
       </div>
--- src/Componoent/ShowCertification.jsx
+++ src/Componoent/ShowCertification.jsx
@@ -8,8 +8,24 @@ export default function ShowCertification() {
       <div className="mt-52  ml-36">
         <p className="sm:text-6xl text-4xl font-thin mb-7 ">Expiryanse:</p>
       </div>
-      <CertificationCard />
-      <CertificationCard />
+      <CertificationCard
+        img="http://localhost:3000/Image/portfolio.PNG"
+        link="https://chra-o.github.io/chra_othman-Portfolio-/"
+        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+        html="https://cdn-icons.flaticon.com/png/128/721/premium/721790.png?token=exp=1643198609~hmac=f8751c9ebe65296bbb0457d4c7548675"
+        css=" https://cdn-icons-png.flaticon.com/512/732/732007.png"
+      
+        desc="Portpolio by Only html and css"
+      />
+      <CertificationCard
+        img="http://localhost:3000/Image/portfolio.PNG"
+        link="https://github.com/chra-O/Php-sale-shop"
+        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+        html="https://cdn-icons.flaticon.com/png/128/721/premium/721790.png?token=exp=1643198609~hmac=f8751c9ebe65296bbb0457d4c7548675"
+        css=" https://cdn-icons-png.flaticon.com/512/5968/5968671.png"
+        php="https://cdn-icons-png.flaticon.com/512/569/569797.png"
+        desc="Portpolio by Only html and css"
+      />
       <CertificationCard />
       <CertificationCard />
       <CertificationCard />
