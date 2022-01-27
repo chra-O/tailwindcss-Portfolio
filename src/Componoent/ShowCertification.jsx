300d3c95f8fbaa54194f6481df48a5d33f32fd5f
Chra <chra.osman99@gmail.com>
2022-01-27 14:46:41 +0300

    addinf data to props

--- src/Componoent/ShowCertification.jsx
+++ src/Componoent/ShowCertification.jsx
@@ -3,35 +3,65 @@ import CertificationCard from "./CertificationCard";
 
 export default function ShowCertification() {
   return (
-    <div className="mt-20 border-red-100 border-double  border-r-8 mr-4 ml-28">
+    <div className=" border-red-100 border-double  border-r-8 mr-4 ml-28">
       {" "}
-      <div className="mt-52  ml-36">
+      <div className="mt-24 ml-36">
         <p className="sm:text-6xl text-4xl font-thin mb-7 ">Expiryanse:</p>
       </div>
+   
       <CertificationCard
-        img="http://localhost:3000/Image/portfolio.PNG"
-        link="https://chra-o.github.io/chra_othman-Portfolio-/"
-        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
-        html="https://cdn-icons.flaticon.com/png/128/721/premium/721790.png?token=exp=1643198609~hmac=f8751c9ebe65296bbb0457d4c7548675"
-        css=" https://cdn-icons-png.flaticon.com/512/732/732007.png"
-      
-        desc="Portpolio by Only html and css"
-      />
-      <CertificationCard
-        img="http://localhost:3000/Image/portfolio.PNG"
+        img="http://localhost:3000/Image/saleshop.PNG"
         link="https://github.com/chra-O/Php-sale-shop"
         git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
         html="https://cdn-icons.flaticon.com/png/128/721/premium/721790.png?token=exp=1643198609~hmac=f8751c9ebe65296bbb0457d4c7548675"
         css=" https://cdn-icons-png.flaticon.com/512/5968/5968671.png"
         php="https://cdn-icons-png.flaticon.com/512/569/569797.png"
+        desc="sale shop with bootstrap and php"
+      />
+      <CertificationCard
+        img="http://localhost:3000/Image/earth.PNG"
+        link="https://github.com/chra-O/earthquakes-app"
+        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+        html="https://cdn-icons.flaticon.com/png/512/3074/premium/3074150.png?token=exp=1643284372~hmac=97b40dd9fc68bfd80e877cc262f91aa4"
+        css=" https://cdn-icons-png.flaticon.com/512/5968/5968671.png"
+        php="https://cdn-icons.flaticon.com/png/512/4166/premium/4166099.png?token=exp=1643283466~hmac=4d76bf67854c9e32cf3e21f6628e76f4"
+        desc="earthquakes app with SGS API"
+      />
+      <CertificationCard
+        img="http://localhost:3000/Image/ecommerse.PNG"
+        link="https://github.com/chra-O/ecommerce"
+        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+        html="https://cdn-icons.flaticon.com/png/512/3074/premium/3074150.png?token=exp=1643284372~hmac=97b40dd9fc68bfd80e877cc262f91aa4"
+        css=" https://cdn-icons-png.flaticon.com/512/5968/5968671.png"
+        php="https://icon-library.com/images/d84be0039d.png"
+        desc=" ecommerce (Backend and frontend) "
+      />
+      <CertificationCard
+        img="http://localhost:3000/Image/raeactodo.PNG"
+        link="https://github.com/chra-O/ToDo-React-Redux"
+        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+        html="https://cdn-icons.flaticon.com/png/512/3074/premium/3074150.png?token=exp=1643284372~hmac=97b40dd9fc68bfd80e877cc262f91aa4"
+        css=" https://cdn-icons-png.flaticon.com/512/5968/5968671.png"
+        php="https://cdn.iconscout.com/icon/free/png-256/redux-3445829-2878755.png"
+        desc="React to do list  "
+      />
+      <CertificationCard
+        img="http://localhost:3000/Image/weather.PNG"
+        link="https://github.com/chra-O/Weather-App"
+        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+        html="https://cdn-icons.flaticon.com/png/512/3074/premium/3074150.png?token=exp=1643284372~hmac=97b40dd9fc68bfd80e877cc262f91aa4"
+        css=" https://cdn-icons-png.flaticon.com/512/5968/5968671.png"
+        php="https://cdn-icons.flaticon.com/png/512/4166/premium/4166099.png?token=exp=1643283466~hmac=4d76bf67854c9e32cf3e21f6628e76f4"
+        desc="Weather app   "
+      />
+         <CertificationCard
+        img="http://localhost:3000/Image/htmlprofile.PNG"
+        link="https://chra-o.github.io/chra_othman-Portfolio-/"
+        git="https://cdn-icons.flaticon.com/png/128/3128/premium/3128308.png?token=exp=1643198514~hmac=0dec76e1ea7f0622187df7f8346c37b5"
+        html="https://cdn-icons.flaticon.com/png/128/721/premium/721790.png?token=exp=1643198609~hmac=f8751c9ebe65296bbb0457d4c7548675"
+        css=" https://cdn-icons-png.flaticon.com/512/732/732007.png"
         desc="Portpolio by Only html and css"
       />
-      <CertificationCard />
-      <CertificationCard />
-      <CertificationCard />
-      <CertificationCard />
-      <CertificationCard />
-      <CertificationCard />
     </div>
   );
 }
